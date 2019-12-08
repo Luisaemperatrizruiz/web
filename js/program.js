5eb4b3e60f9174900f36a5e063e526d3ec405b9a
Luisaemperatrizruiz <Luisaeruiz@gmail.com>
2019-12-08 00:51:05 -0600

    Inicial

---
+++ js/program.js
@@ -0,0 +1,14 @@
+$("#PDF").click(function(){
+	window.location = "./pages/galeriapdf.html";
+});
+
+$("#Galeria").click(function(){
+
+	window.location = "./pages/galeria.html";
+});
+
+$("#Salir").click(function(){
+	window.location = "../Index.html";
+});
+
+
---
+++ js/program2.js
@@ -0,0 +1,8 @@
+$("#Salir").click(function(){
+	window.location = "../Index.html";
+});
+
+
+$("#pdf").click(function(){
+	window.location = "../pages/galeriapdf.html";
+});
